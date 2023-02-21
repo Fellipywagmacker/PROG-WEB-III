8e76a984404c7f0396389c24774ea49ed883b8f9
Fellipywagmacker <91857091+Fellipywagmacker@users.noreply.github.com>
2023-02-20 21:13:59 -0300

    Create aula3.js

---
+++ aula3.js
@@ -0,0 +1,60 @@
+/* Código 1 
+
+let idade = 17; // Minha idade 
+
+let idadeMaxima = 80; // Minha idade máxima 
+
+let lancheDia = 1; // Lanches por dia 
+
+let anosRestante = idadeMaxima - idade; // Calculo para saber quantos anos ainda vou viver 
+
+let qtdlanches = anosRestante * 365 * lancheDia; // Calculando a quantidade de lanches que vou comer 
+
+// Output de quantos lanches vou precisar 
+console.log(`Você precisará de ${qtdlanches} lanches para durar até a velhice de ${idadeMaxima} anos.`);
+*/
+
+
+/* Código 2 
+
+let raio = 15; // Variável com o raio do circulo 
+
+let circunferencia = 2 * Math.PI * raio; // Calculo da circunferência usando Math.PI 
+
+// Output com a circunferência 
+console.log("A circunferência tem " + circunferencia + ".");
+
+// Calculando a area do raia 
+let area = Math.PI * (raio ** 2);
+
+// Output com o resultado da area 
+console.log("A área é " + area + ".");
+*/
+
+/* Código 3 
+let idioma = "es"; // Definindo o idioma 
+
+
+if (idioma === "fr") {
+  console.log("Bonjour tout le monde!"); // Se(if) o idioma for igual a "fr" imprima "Bonjour tout le monde!" 
+
+} else if (idioma === "es") {
+  console.log("Hola, Mundo!"); // Senao se(else if) o idioma for igual a "es" imprima "Hola, Mundo!"
+
+} else {
+  console.log("Hello World!"); // Senha(else) o idioma for diferente de "es" e "fr" imprima "Hello World!" 
+}
+*/
+
+/* Código 4 
+
+let linhas = 8; // Quantidade de linhas
+
+for (let i = 1; i <= linhas; i++) {
+  let linha = "";
+  for (let j = 1; j <= i; j++) {
+    linha += j;
+  }
+  console.log(linha);
+}
+*/
